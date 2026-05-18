fb3dab11f264843969920fc91798b78965920981
Claudia Chen <chen_yu1@student.kings.edu.au>
2026-05-19 08:20:52 +1000

    database - setup

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(
     __name__,
     template_folder='templates'
 )
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def login():
@@ -16,6 +32,52 @@ def signup():
 @app.route('/forgot_password')
 def forgot_password():
     return render_template('forgot-password.html')
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
+
+
+
+#sign up user route
+@app.route('/signup_user', methods=['POST'])
+def signup_user():
+
+    email = request.form['email']
+    password = request.form['password']
+
+    existing_user = User.query.filter_by(email=email).first()
+
+    if existing_user:
+        return "Email already registered."
+
+    user = User(
+        email=email,
+        password=password
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(url_for('homepage'))
+    
+#login user route
+@app.route('/login_user', methods=['POST'])
+def login_user():
+
+    email = request.form['email']
+    password = request.form['password']
+
+    user = User.query.filter_by(
+        email=email,
+        password=password
+    ).first()
+
+    if user:
+        return redirect(url_for('homepage'))
+
+    return "Invalid email or password"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
