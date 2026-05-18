1fb1109515b1d5f55808cab3eee9997e3a1fe964
Claudia Chen <chen_yu1@student.kings.edu.au>
2026-05-18 16:42:41 +1000

    frontend - sign up and forget password page

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(
     __name__,
-    template_folder = "frontend/templetes"
+    template_folder='templates'
 )
 
 @app.route('/')
 def login():
     return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template('forgot-password.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
